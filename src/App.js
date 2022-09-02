fc8ff556d3f72b5a422eac3f4766dd0bae99b6c3
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-02 19:50:45 +0600

    route setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+
+      </Routes>
 
     </div>
   );
