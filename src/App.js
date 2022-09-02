95ae6d3cad6c42e9caa7b7ca60011c24bd2b8c09
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-02 23:18:15 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
 import Home from './components/Home/Home';
+import Header from './components/Shared/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,6 +3,8 @@ import React from 'react';
 const Product = () => {
     return (
         <div>
+            <h2>Product</h2>
+
 
         </div>
     );
