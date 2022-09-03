349c313c91429a6b31f8138efb4d4aeb9fb66a91
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-03 07:13:00 +0600

    set dynamic year

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
 import Home from './components/Home/Home';
+import Footer from './components/Shared/Footer/Footer';
 import Header from './components/Shared/Header/Header';
+import NotFound from './components/Shared/NotFound/NotFound';
 
 function App() {
   return (
@@ -12,9 +14,9 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
-
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
-
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
     return (
-        <div>
-
+        <div className='py-5 text-center bg-dark text-white'>
+            <p>Genius Car &copy; {year}</p>
         </div>
     );
 };
