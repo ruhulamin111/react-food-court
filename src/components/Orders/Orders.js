1ac000255cae0f56decb34d00bc3bf47deccac6e
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-08 16:25:57 +0600

    done this site

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,23 +1,34 @@
 import axios from 'axios';
+import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { Navigate, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const Orders = () => {
     const [user] = useAuthState(auth)
     const email = user.email;
     const [order, setOrder] = useState([])
+    const navigate = useNavigate()
     useEffect(() => {
         const orderList = async () => {
-            const { data } = await axios.get(`http://localhost:5000/order?email=${email}`, {
-                headers: {
-                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            try {
+                const { data } = await axios.get(`http://localhost:5000/order?email=${email}`, {
+                    headers: {
+                        authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                    }
+                })
+                setOrder(data)
+            }
+            catch (error) {
+                if (error.response.status === 401 || error.response.status === 403) {
+                    signOut(auth)
+                    navigate('/signin')
                 }
-            })
-            setOrder(data)
+            }
         }
         orderList()
-    }, [email])
+    }, [email, navigate])
 
     return (
         <div>
